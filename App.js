9e3fbfe6cbb8edc2a4d119116918d4a26e9a5099
Ruth Chisom <ndukwe100805043@gmail.com>
2023-04-03 18:56:06 +0100

    set up first page

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import StackScreens from "./navigation/StackScreens";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <StackScreens />
+    </NavigationContainer>
+);
 }
 
 const styles = StyleSheet.create({
---
+++ externalData.js
@@ -0,0 +1,33 @@
+const categories = [
+    {
+      id: 0,
+      title: "Nice food",
+      image: require("../assets/images/food4.jpg"),
+      price: 20,
+      rating: 2
+    },
+    {
+      id: 1,
+      title: "How to start investing",
+      image: require("../assets/images/food3.jpg"),
+      price: 25,
+      rating: 3.7
+    },
+    {
+      id: 2,
+      title: "How to predict the candlestick",
+      image: require("../assets/images/food2.jpg"),
+      price: 15,
+      rating: 4
+    },
+    {
+      id: 3,
+      title: "Is trading safe for newbies",
+      image: require("../assets/images/food1.jpg"),
+      price: 30,
+      rating: 5
+    },
+  ];
+  
+  export { categories };
+  
